b2c00536779ab9d512005e1aa9d39fd4a66fdd37
ppbstukr <108387059+ppbstukr@users.noreply.github.com>
2024-04-23 09:36:59 +0900

    0423

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-const calculator = {
-    add: function(a, b){
-        console.log(a + b)
-    },
-    minus: function(a, b){
-        console.log(a - b)
-    },
-    div: function(a, b){
-        console.log(a / b)
-    },
-    muliti: function(a, b){
-        console.log(a * b)
-    },
-    power: function(a, b){
-        console.log(a ** b)
-    },
-    
-    
-}
+const age = prompt("How old are you?")
 
-calculator.add(2334, 1244)
-calculator.minus(45, 444)
-calculator.muliti(124, 42424)
-calculator.power(14, 234)
-calculator.div(111111111114, 4123)
+console.log(age)
