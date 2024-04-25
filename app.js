d6ecee9b0b181e4de7dfbabb4eba85136e294117
ppbstukr <108387059+ppbstukr@users.noreply.github.com>
2024-04-25 17:46:55 +0900

    3

--- app.js
+++ app.js
@@ -1,47 +1,22 @@
-const h1 = document.querySelector("#title");
+const ppb = document.querySelector("#title");
 
-console.dir(h1);
-h1.innerText = "hi ppb";
 
-function handleTitleClick() {
-  h1.style.color = "blue";
-}
-
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function copy() {
-  alert("copier!");
-}
-
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.onmouseleave = handleMouseLeave;
-
-function handelWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-function handleWindowOffLine() {
-  alert("Not conected wifi");
-}
-function handleWindowOnLine() {
-  alert("good");
-}
-
-function clickMouseRight() {
-  h1.innerText = "Mouse right";
-}
-window.addEventListener("resize", handelWindowResize);
-window.addEventListener("copy", copy);
-window.addEventListener("offline", handleWindowOffLine);
-window.addEventListener("online", handleWindowOnLine);
-
-function mouseRight() {
-  h1.innerText = "mouse right";
-}
-window.addEventListener("contextmenu", mouseRight);
+const superEventHandler = {
+    mouseEnter: function () {
+      ppb.innerText = "ppb mouse enter!";
+    },
+    mouseLeave: function () {
+      ppb.innerText = "ppb mouse leave!";
+    },
+    resize: function () {
+      ppb.innerText = "ppb resize!";
+    },
+    rightMouse: function () {
+      ppb.innerText = "ppb rightMouse!";
+    },
+  };
+  
+  ppb.addEventListener("mouseenter", superEventHandler.mouseEnter);
+  ppb.addEventListener("mouseleave", superEventHandler.mouseLeave);
+  window.addEventListener("resize", superEventHandler.resize);
+  window.addEventListener("contextmenu", superEventHandler.rightMouse);
