03afb595d76b60e4284bbcc40fa12409f9a06799
ppb <108387059+ppbstukr@users.noreply.github.com>
2024-05-01 00:55:09 +0900

    1

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 const ppb = document.querySelector("#title");
 
+function handleTitleClick(){
+  ppb.classList.toggle("clicked")
+};
 
-const superEventHandler = {
-    mouseEnter: function () {
-      ppb.innerText = "ppb mouse enter!";
-    },
-    mouseLeave: function () {
-      ppb.innerText = "ppb mouse leave!";
-    },
-    resize: function () {
-      ppb.innerText = "ppb resize!";
-    },
-    rightMouse: function () {
-      ppb.innerText = "ppb rightMouse!";
-    },
-  };
-  
-  ppb.addEventListener("mouseenter", superEventHandler.mouseEnter);
-  ppb.addEventListener("mouseleave", superEventHandler.mouseLeave);
-  window.addEventListener("resize", superEventHandler.resize);
-  window.addEventListener("contextmenu", superEventHandler.rightMouse);
+ppb.addEventListener("click", handleTitleClick);
