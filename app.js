4d200506b58eefbd847cbb3ea5d8752502df1a37
ppbstukr <108387059+ppbstukr@users.noreply.github.com>
2024-05-09 15:02:05 +0900

    t

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const loginForm = document.querySelector("#login-form")
-const loginInput = loginForm.querySelector("input")
-const loginButton = loginForm.querySelector("button")
-function(){
+const loginForm = document.querySelector("#app");
+const loginInput = document.querySelector("#app input");
+const loginButton = document.querySelector("#app button");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "";
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-};
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+loginForm.classList.remove(HIDDEN_CLASSNAME);
+loginForm.addEventListener("submit", onLoginSubmit)
+} else {}
